bb9d3110d3f8faf9596ff9959ccd62f2bedccd36
Ketan <ketantoo@gmail.com>
2020-10-04 01:13:22 +0530

    isJS and calculate difference

--- October 2020/K-diff Pairs in an Array.js
+++ October 2020/K-diff Pairs in an Array.js
@@ -1,4 +1,5 @@
 /**
+ * https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3482/
  * It workd but TLE :( 47/59 passed
  * Github: theketan
  */
