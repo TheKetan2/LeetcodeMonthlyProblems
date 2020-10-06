75e7874b7367fb5a0d1e3651fbb2303780748636
Ketan <ketantoo@gmail.com>
2020-10-06 15:47:33 +0530

    valid paranthesis

---
+++ validParanthesis.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+/*
+ * https://leetcode.com/problems/valid-parentheses/submissions/
+ * GiHub: TheKetan2
+ */
+var isValid = function (s) {
+  let stack = [];
+  for (char of [...s]) {
+    if (!stack) {
+      stack.push(char);
+    } else {
+      if (char === "]" && stack[stack.length - 1] === "[") {
+        stack.pop();
+      } else if (char === "}" && stack[stack.length - 1] === "{") {
+        stack.pop();
+      } else if (char === ")" && stack[stack.length - 1] === "(") {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    }
+  }
+  console.log(stack);
+  return stack.length === 0;
+};
