bd7f552f39e92664a29cd95dc676467e4d90b641
Ketan <ketantoo@gmail.com>
2020-11-03 11:49:29 +0530

    restoreString

---
+++ restoreString.js
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode.com/problems/shuffle-string/submissions/
+ * github: theketan2
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function (s, indices) {
+  let sol = Array.from({ length: s.length }, () => 0);
+  for (let i = 0; i < s.length; i++) {
+    sol[indices[i]] = s[i];
+  }
+  return sol.join("");
+};
