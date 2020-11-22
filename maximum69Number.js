650613d776ce61f864fcf165b6b52bc60fa970cf
Ketan <ketantoo@gmail.com>
2020-11-23 02:33:53 +0530

    maximum69Number

--- maximum69Number.js
+++ maximum69Number.js
@@ -9,3 +9,19 @@ var maximum69Number = function (num) {
   }
   return Math.max(...sol);
 };
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number = function (num) {
+  let sol = 0;
+  str = [...num.toString()];
+  for (let i = 0; i < str.length; i++) {
+    let temp = [...str];
+    if (temp[i] === 9) temp[i] = 6;
+    else temp[i] = 9;
+    if (sol < parseInt(temp.join(""))) sol = parseInt(temp.join(""));
+  }
+  return sol;
+};
