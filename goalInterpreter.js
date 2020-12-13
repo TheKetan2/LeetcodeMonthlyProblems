c5f6d3ad5b75c78a6fad03227dbc1741de5d4157
Ketan <ketantoo@gmail.com>
2020-12-13 16:06:26 +0530

    interpreter

---
+++ goalInterpreter.js
@@ -0,0 +1,15 @@
+/**
+ * https://leetcode.com/problems/goal-parser-interpretation/submissions/
+ * github: theketan2
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function(command) {
+    while(command.includes("()")){
+          command = command.replace("()", "o")
+          }
+    while(command.includes("(al)")){
+          command = command.replace("(al)", "al")
+          }
+    return command;
+};
