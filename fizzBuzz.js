9002b3c570111a0e32cfbee8596c4b2c6d0f35fe
Ketan <ketantoo@gmail.com>
2021-01-28 13:20:12 +0530

    fizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,21 @@
+/**
+ * url: https://leetcode.com/problems/fizz-buzz
+ * github: theketan2
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+  let sol = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      sol.push("FizzBuzz");
+    } else if (i % 3 == 0) {
+      sol.push("Fizz");
+    } else if (i % 5 == 0) {
+      sol.push("Buzz");
+    } else {
+      sol.push(i.toString());
+    }
+  }
+  return sol;
+};
