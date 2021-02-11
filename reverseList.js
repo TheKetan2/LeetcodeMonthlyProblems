69b45d63c9337a543f77d773dc7d3a265d39845f
Ketan <ketantoo@gmail.com>
2021-02-11 20:50:18 +0530

    reverseList

---
+++ reverseList.js
@@ -0,0 +1,10 @@
+var reverseList = function (head) {
+  let previous = null;
+  while (head) {
+    let temp = head;
+    head = head.next;
+    temp.next = previous;
+    previous = temp;
+  }
+  return previous;
+};
