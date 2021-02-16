8bd050fb8839a4b03224b783f3698f761ead8473
Ketan <ketantoo@gmail.com>
2021-02-16 20:28:48 +0530

    stringMatching

---
+++ stringMatching.js
@@ -0,0 +1,17 @@
+/**
+ * github: theketan2
+ * https://leetcode.com/problems/string-matching-in-an-array
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var stringMatching = function (words) {
+  let sol = [];
+  words.sort((a, b) => a.length - b.length);
+  for (let i = 0; i < words.length - 1; i++) {
+    let filter = words.slice(i + 1).filter((word) => word.includes(words[i]));
+    if (filter.length) {
+      sol.push(words[i]);
+    }
+  }
+  return sol;
+};
