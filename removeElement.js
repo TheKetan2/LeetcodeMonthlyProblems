89c05dd08f3928ad0f76c967848b3da14b3c4e15
Ketan <ketantoo@gmail.com>
2021-02-24 14:15:43 +0530

    removeElement

---
+++ removeElement.js
@@ -0,0 +1,18 @@
+/**
+ * https://leetcode.com/problems/remove-element/
+ * github: theketan2
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+      console.log(nums);
+    }
+  }
+
+  return nums.length;
+};
