57090ecc262e371b1a4ed6e43fb0053d845afed6
Ketan <ketantoo@gmail.com>
2021-03-03 10:52:22 +0530

    findErrorNums

--- findErrorNums.js
+++ findErrorNums.js
@@ -1,6 +1,6 @@
 /**
  * gtihub: theketan2
- * https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3658/
+ * https://leetcode.com/problems/set-mismatch
  * @param {number[]} nums
  * @return {number[]}
  */
