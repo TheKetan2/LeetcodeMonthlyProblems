707b47615ff39a4904811ae00a5420addea71c3b
Ketan <ketantoo@gmail.com>
2021-03-19 13:34:31 +0530

    containsNearbyDuplicate

---
+++ containsNearbyDuplicate.js
@@ -0,0 +1,17 @@
+/**
+ * github: theketan2
+ * https://leetcode.com/problems/contains-duplicate-ii/
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j] && j - i <= k) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
