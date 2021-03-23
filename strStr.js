48f1de46a230a57989ae7643bcc24f8e2ccb8208
Ketan <ketantoo@gmail.com>
2021-03-23 18:04:01 +0530

    strStr

---
+++ strStr.js
@@ -0,0 +1,15 @@
+/**https://leetcode.com/problems/implement-strstr/submissions/
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  let len = needle.length;
+  if (len == 0) return 0;
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack.substr(i, len) === needle) {
+      return i;
+    }
+  }
+  return -1;
+};
