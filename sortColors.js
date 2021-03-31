e5368904de548c82480856e88814bdf62d85184d
Ketan <ketantoo@gmail.com>
2021-03-31 12:08:14 +0530

    sprtColors

---
+++ sortColors.js
@@ -0,0 +1,9 @@
+/**
+ * github: theketan2
+ * https://leetcode.com/problems/sort-colors/
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+  nums.sort((a, b) => a - b);
+};
