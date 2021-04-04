888cacbc03ad5a38c35259194c8a21ce3c035017
Ketan <ketantoo@gmail.com>
2021-04-04 16:09:16 +0530

    checkIfExist

---
+++ checkIfExist.js
@@ -0,0 +1,18 @@
+/**
+ * github: theketan2
+ * https://leetcode.com/explore/learn/card/fun-with-arrays/527/searching-for-items-in-an-array/3250/
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let temp = [...arr];
+    temp.splice(i, 1);
+    console.log(temp);
+    if (temp.includes(arr[i] + arr[i])) {
+      console.log(arr[i]);
+      return true;
+    }
+  }
+  return false;
+};
