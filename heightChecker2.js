f4666e7a16655b7d328e2d2ef46d686f42a0801d
Ketan <ketantoo@gmail.com>
2021-04-08 20:31:16 +0530

    heightChecker

---
+++ heightChecker2.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function (heights) {
+  let temp = [].concat(heights);
+  heights.sort((a, b) => parseInt(a) - parseInt(b));
+  console.log(heights);
+  let sol = 0;
+  for (let i in temp) {
+    console.log(heights[i], " == ", temp[i]);
+    if (temp[i] !== heights[i]) sol++;
+  }
+  return sol;
+};
