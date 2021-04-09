1a1ff1555fc6573c12e66fdd041c722184c2661a
Ketan <ketantoo@gmail.com>
2021-04-09 20:37:46 +0530

    truncateSentence

---
+++ truncateSentence.js
@@ -0,0 +1,10 @@
+/**
+ * github: theketan2
+ * https://leetcode.com/problems/truncate-sentence/
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var truncateSentence = function (s, k) {
+  return s.split(" ").slice(0, k).join(" ");
+};
