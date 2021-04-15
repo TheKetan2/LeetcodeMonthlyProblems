f6290c6d4ea1ea9d827e4b62f880249f60965b09
Ketan <ketantoo@gmail.com>
2021-04-15 22:50:17 +0530

    isPalindrome

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,29 +1,10 @@
 /**
  * github: theketan2
- * https://leetcode.com/problems/palindrome-linked-list/submissions/
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
+ * https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/883/
+ * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function (head) {
-  let sol = [];
-  // console.log(head.val)
-  while (head.next !== null) {
-    sol.push(head.val);
-    head = head.next;
-  }
-  sol.push(head.val);
-  // console.log(sol)
-  for (let i = 0, j = sol.length - 1; i < j; i++, j--) {
-    if (sol[i] !== sol[j]) {
-      return false;
-    }
-  }
-  return true;
+var isPalindrome = function (s) {
+  let str = s.toLowerCase().replace(/[^a-z0-9]+/gi, "");
+  return str === str.split("").reverse().join("");
 };
